b3a27c3f9da14671a3c326228d547799deae707d
MrVain <mrvainirc@proton.me>
2024-10-26 07:00:59 +0200

    Update nickcollator.c
    now the right license

--- nickcollator/nickcollator.c
+++ nickcollator/nickcollator.c
@@ -3,23 +3,20 @@
  * 
  * Description:
  * This module enables your UnrealIRCd server to handle nick names across
- * various scripts and languages by using Unicode encoding for consistent character handling.
- * 
- * License: GNU General Public License v3.0
- * 
- * Copyright (c) 2024
+ * various scripts and languages by using Unicode encoding for consistent
+ * character handling.
  *
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
+ * GNU Affero General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
+ * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  *
  * Links to external resources:
@@ -27,8 +24,6 @@
  *   https://gitgud.malvager.net/Wazakindjes/unrealircd_mods
  * - ICU Project: https://icu.unicode.org/
  * - UnrealIRCd: https://www.unrealircd.org/
- * 
- *
  */
 
 #include "unrealircd.h"
